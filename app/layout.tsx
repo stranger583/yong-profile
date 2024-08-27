6021e1886aa05b0ce6d895d24bb91ef8a5a9a043
stranger583 <zed042760@gmail.com>
2024-08-27 23:51:50 +0800

    chore:移除fish

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import { cN } from "@utils/tw-marge";
 import { GoogleAnalytics } from '@next/third-parties/google'
+import Head from "next/head";
+import Script from "next/script";
 export const metadata: Metadata = {
   title: "Yong | About",
   description: "Yong Hua Chen's Profile",
@@ -22,7 +24,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cN(spaceGrotesk.className,'relative')}>{children}</body>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-GD7MT0KN8F" />
+      <Script
+        id="google-analytics"
+        async
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-GD7MT0KN8F');
+            `,
+        }}
+      />
+      <body className={cN(spaceGrotesk.className, 'relative')}>{children}</body>
       <GoogleAnalytics gaId="G-GD7MT0KN8F" />
     </html>
   );
